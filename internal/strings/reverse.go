1ef9f97abd3c7774897568c2d4065e1ad01e3230
Chastity Blackwell <blackisis@gmail.com>
2026-01-29 07:27:59 -0600

    bug: testing workflow change

--- internal/strings/reverse.go
+++ internal/strings/reverse.go
@@ -1,7 +1,7 @@
 package strings
 
-// Reverse returns the input string with its characters in reverse order.
-func Reverse(s string) string {
+// ReverseString returns the input string with its characters in reverse order.
+func ReverseString(s string) string {
 	runes := []rune(s)
 	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
 		runes[i], runes[j] = runes[j], runes[i]
